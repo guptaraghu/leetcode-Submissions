6dffc498e1e0b6d5be411f2c506124ecc1cdf023
Raghav Gupta <guptaraghav059@gmail.com>
2022-08-02 22:19:55 +0530

    Time: 40 ms (37.97%), Space: 19.8 MB (5.91%) - LeetHub

--- 135-candy/135-candy.cpp
+++ 135-candy/135-candy.cpp
@@ -12,16 +12,12 @@ public:
         for(int i =1;i<ratings.size();i++)
         {
             if(ratings[i]>ratings[i-1])
-            {
                 left_ratings[i]=left_ratings[i-1]+1;
-            }
         }
         for(int i=ratings.size()-2; i>=0;i--)
         {
             if(ratings[i]>ratings[i+1])
-            {
                 right_ratings[i]=right_ratings[i+1]+1;
-            }
         }
         for(int i=0;i<right_ratings.size();i++)
         {
